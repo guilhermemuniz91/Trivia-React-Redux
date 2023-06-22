de0c70e67a4bb6d08900397731d805a35fb946bd
Talia Fretta <frettatalia@gmail.com>
2023-06-22 19:38:42 -0300

    req 5 em andamento

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  state = {
+    imgGravatar: '',
+  };
+
+  gravatar = async () => {
+    const { email } = this.props;
+    const request = await fetch(`https://www.gravatar.com/avatar/${email}`);
+    const data = request.json();
+  };
+
   render() {
     const { name } = this.props;
     return (
       <thead>
         <p data-testid="header-score" />
+        <img data-testid="header-profile-picture" src="" />
         <p data-testid="header-player-name">{name}</p>
       </thead>
     );
@@ -21,6 +32,7 @@ Header.propTypes = {
 function mapStateToProps(state) {
   return {
     name: state.player.name,
+    email: state.player.gravatarEmail,
     // score: state.player.score,
   };
 }
