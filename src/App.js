f7fc4ad23c502b70762b451359bf43f8774d7b12
Joao Lucas Camargo <j.lucascamargo@gmail.com>
2023-06-26 09:17:57 -0300

    Requisitos 6 7 e 8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './Global.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Settings from './components/Settings';
